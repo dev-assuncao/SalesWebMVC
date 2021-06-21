04ec2692db5f256572814595771790ad5132a4d2
dev-assuncao <davisson.assuncao@protonmail.com>
2021-06-21 20:38:10 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException : ArgumentException
+    {
+        public DbConcurrencyException(string message) : base (message)
+        {
+        }
+    }
+}
